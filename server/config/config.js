8a10b798f5b23edeb6dcb145a6ebdc29b81247c2
samidz26 <samizaouiadz@gmail.com>
2026-09-05 20:06:45 +0100

    Add configuration file for server settings

---
+++ server/config/config.js
@@ -0,0 +1,12 @@
+const config = {
+    port: process.env.PORT || 3000,
+
+    projectName: "S-LIVE",
+
+    tiktok: {
+        username: "",
+        connected: false
+    }
+};
+
+module.exports = config;
